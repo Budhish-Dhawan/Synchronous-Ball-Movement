6096b334391db941e336e45e314183b2492a5f95
Budhish-Dhawan <budhishdhawan@gmail.com>
2021-01-27 15:57:17 +0530

    synchronous ball movement

--- sketch.js
+++ sketch.js
@@ -1,29 +1,57 @@
-var ball;
+var hypnoticBall, database;
+var position;
 
 function setup(){
+    database = firebase.database();
+
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+
+    var hypnoticBallPosition = database.ref('ball/position');
+    //Everytime a change in the database values of position (reference) happens, the readPosition function is called.
+    //If there is any error in reading the values in the database, the showError function is called.
+    hypnoticBallPosition.on("value", readPosition, showError);
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+    if(position!==undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
+        drawSprites();
+
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
+   
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        x:position.x + x,
+        y:position.y + y
+    })
+}
+
+
+function readPosition(data){
+    //In the readPosition function we can read the position of the value in the database
+    position = data.val();
+    //We assign the x and y values of the ball position in the database to the ball sprite
+    hypnoticBall.x = position.x;
+    hypnoticBall.y = position.y;
+}
+
+function showError(){
+    console.log("Error in writing to the database");
 }
